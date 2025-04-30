d8bdea263946ec55698defd7e211d7865dd6ad21
bezmuth <benkel97@protonmail.com>
2025-04-30 21:38:45 +0100

    Remove the example memory mapper

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,6 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    // write the string New! to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(300).write_volatile(0x_f021_f077_f065_f04e) };
-
     #[cfg(test)]
     test_main();
 
